a854fc257ac94a1f6aeeaa5f318e096bed289f20
JasonxPortfolio <97375007+JasonxPortfolio@users.noreply.github.com>
2023-11-18 23:50:34 -0500

    Random number generator

--- first.py
+++ first.py
@@ -1,3 +1,7 @@
-phrase = "Stop it"
-for char in phrase:
-  print(char)
+optionA = "Ascendant Frontier"
+optionB = "Band of Brothers"
+
+import random
+
+random_float = random.uniform(0.0, 1.6)  # Generates a random float between 0.0 and 1.0 (inclusive)
+print(random_float) # 1.2732697746309614
