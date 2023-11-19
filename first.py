0200761cddee5afc295156d1cf31692a57845e20
JasonxPortfolio <97375007+JasonxPortfolio@users.noreply.github.com>
2023-11-18 23:23:12 -0500

    basic for loop

--- first.py
+++ first.py
@@ -1,7 +1,3 @@
-lousy__while_condition = True  # mimics a condition that may always be true
-counter = 0
-while lousy__while_condition:
-    if counter >= 100:
-        break
-    print("Execute block of code", counter)
-    counter += 1
+phrase = "Stop it"
+for char in phrase:
+  print(char)
