0e4ec4569812db2790dd19f9b0b81e8c34fdec50
JasonxPortfolio <97375007+JasonxPortfolio@users.noreply.github.com>
2023-11-18 23:50:48 -0500

    Iterate list

--- first.py
+++ first.py
@@ -1,7 +1,3 @@
-optionA = "Ascendant Frontier"
-optionB = "Band of Brothers"
-
-import random
-
-random_float = random.uniform(0.0, 1.6)  # Generates a random float between 0.0 and 1.0 (inclusive)
-print(random_float) # 1.2732697746309614
+my_list = [1, 2, 3, 4, 5]
+for item in my_list:
+    print(item)
